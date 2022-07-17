6ab9d37121bed00552f4344f3af4b57a0ce61694
glendc <glen.decauwsemaecker@otainsight.com>
2022-07-17 22:36:04 +0200

    add copyright notes

--- geopaint-editor/src/lib/pages/mod.rs
+++ geopaint-editor/src/lib/pages/mod.rs
@@ -1,3 +1,19 @@
+// Plabayo — geopaint.app
+// Copyright (C) 2022 Glen Henri J. De Cauwsemaecker
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use yew::prelude::*;
 
 mod home;
@@ -57,7 +73,9 @@ fn page(page: Page, content: Html) -> Html {
                         <a href="https://plabayo.tech/" rel="nofollow">{ "Plabayo" }</a>
                         { ". Source code available at " }
                         <a href="https://github.com/plabayo/geopaint.app" rel="nofollow">{ "GitHub" }</a>
-                        { "." }
+                        { ". This project is licensed under " }
+                        <a href="https://github.com/plabayo/geopaint.app/blob/main/LICENSE" rel="nofollow">{ "the GNU GPLv3 License" }</a>
+                        { "."}
                     </p>
                 </div>
             </footer>
